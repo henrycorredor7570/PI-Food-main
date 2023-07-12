70196632001b7e064ef39fb6d8db8d8a220936e5
henrycorredor7570 <henry.corredor7570@gmail.com>
2023-07-11 22:04:08 -0500

    avances

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo.
+  // defino el modelo
   sequelize.define('Diet', {
     id:{
       type: DataTypes.UUID,//UUID es una combinacion de numeros, letras y guiones(codigo alfanumerico)
