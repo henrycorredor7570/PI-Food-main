a9b7915409771f075fb7e85006b38878e84c94ce
henrycorredor7570 <henry.corredor7570@gmail.com>
2023-07-11 22:03:28 -0500

    avances

--- api/src/routes/dietsRouter.js
+++ api/src/routes/dietsRouter.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const dietsRouter = Router();
 const { getDietsHandler } = require("../handlers/dietsHandler");
 
-// endpoints: ruta de acceso a nuestro backend
+// endpoints: ruta de acceso a nuestro backend.
 dietsRouter.get("/", getDietsHandler);
 
 module.exports = dietsRouter;
