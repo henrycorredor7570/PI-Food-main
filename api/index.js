83d86fe37708b0272381f513a2428ab94d59ead4
henrycorredor7570 <henry.corredor7570@gmail.com>
2023-08-05 09:28:23 -0500

    avances

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {// sincronizar la conexion entre servidor y base de datos
+conn.sync({ force: false }).then(() => {// sincronizar la conexion entre servidor y base de datos.
   server.listen(3002, () => {
     console.log('%s listening at 3002'); // eslint-disable-line no-console
   });
